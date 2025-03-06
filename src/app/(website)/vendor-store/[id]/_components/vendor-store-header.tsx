7474b96e68d615c469b9a6e4bf2768ec3b3ad48b
amitchackroborty04 <amitck803@gmail.com>
2025-03-06 12:27:58 +0600

    develop store review modal

--- src/app/(website)/vendor-store/[id]/_components/vendor-store-header.tsx
+++ src/app/(website)/vendor-store/[id]/_components/vendor-store-header.tsx
@@ -1,9 +1,27 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import StarRating from "@/components/ui/star-rating";
 import Image from "next/image";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { MdOutlineReviews } from "react-icons/md";
+import * as React from "react";
+import { ProvideStarRating } from "@/app/(website)/products/[id]/_components/ProvideStarRatting";
 
 const VendorStoreHeader = () => {
+  const [rating, setRating] = React.useState(0);
+   const [review, setReview] = React.useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (rating === 0) {
+      alert("Please provide a rating.");
+      return;
+    }
+    console.log("Rating:", rating);
+    console.log("Review:", review);
+    setRating(0);
+    setReview("");
+  };
   return (
     <div className="relative ">
       <div
@@ -25,13 +43,13 @@ const VendorStoreHeader = () => {
           className="rounded-[12px] object-cover"
           alt="profile"
         />
-        <h1 className="mt-[20px] text-[48px] font-semibold leading-[57.6px]">
-          License Information
+        <h1 className="mt-[20px] text-[48px] text-[#000000] font-semibold leading-[57.6px]">
+          License Information 
         </h1>
         <div className="flex items-center gap-x-3">
-          <span className="text-[#3D3D3D] text-[16px] font-bold leading-[19.2px]">
+          {/* <span className="text-[#3D3D3D] text-[16px] font-bold leading-[19.2px]">
             12Followers
-          </span>
+          </span> */}
           <StarRating fill={4} outline={1} size={20} />
           <span className="text-[#3D3D3D] text-[16px] font-bold"> (4.0)</span>
         </div>
@@ -51,7 +69,7 @@ const VendorStoreHeader = () => {
               Message
             </Link>
           </Button>
-          <Button
+          {/* <Button
             className="flex items-center gap-x-2 text-[16px] font-bold leading-[19.2px]"
             variant="outline"
           >
@@ -62,9 +80,58 @@ const VendorStoreHeader = () => {
               width={15}
             />
             Message
-          </Button>
+          </Button> */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline" className="bg-white text-gradient dark:text-gradient-pink "  >
+                <span>
+                  <MdOutlineReviews className="text-black "/>
+                </span>
+                Reveiw</Button>
+            </DialogTrigger>
+            <DialogContent className="w-[80%] md:w-[50%]">
+                <form
+                   onSubmit={handleSubmit}
+                   className="flex flex-col gap-4 rounded-lg w-full"
+                 >
+                   <h2 className="text-lg font-semibold text-gradient dark:bg-pinkGradient">Add a review</h2>
+             
+                   <div className="flex gap-2">
+                     <label htmlFor="rating" className="text-base text-neutral-700">
+                       Your rating <span className="text-red-600">*</span>
+                     </label>
+                     <span>
+                       <ProvideStarRating rating={rating} onChange={setRating} />
+                     </span>
+                   </div>
+             
+                   <div className="flex flex-col gap-2">
+                     <label htmlFor="review" className="text-base text-neutral-700">
+                       Your review <span className="text-red-600">*</span>
+                     </label>
+                     <textarea
+                       id="review"
+                       value={review}
+                       onChange={(e) => setReview(e.target.value)}
+                       className="p-3 border rounded-lg min-h-[120px] border-neutral-300 focus:border-green-600 focus:ring-green-600 dark:bg-white dark:text-black"
+                       placeholder="Write your review"
+                       required
+                       aria-required="true"
+                     />
+                   </div>
+             
+                   <Button
+                     type="submit"
+                     className="self-start "
+                   >
+                     Submit
+                   </Button>
+                 </form>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
+    
     </div>
   );
 };
