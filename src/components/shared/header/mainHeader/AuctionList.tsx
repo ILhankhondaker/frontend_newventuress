36ebc1ea1d766516035ba19ce277b65ce7ae3c1c
Monir Hossain <monirhrabby.personal@gmail.com>
2025-03-14 12:27:58 +0600

    a

--- src/components/shared/header/mainHeader/AuctionList.tsx
+++ src/components/shared/header/mainHeader/AuctionList.tsx
@@ -1,20 +1,21 @@
 import { ChevronDown, ChevronUp } from "lucide-react";
 import React, { useState } from "react";
-import AuctionModal from "./AuctionModal";
 
 const AuctionList: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
+
+    console.log(isModalOpen)
   
     const handleItemClick = () => {
       setIsModalOpen(true);
       setIsExpanded(true); // Set expanded to true when the modal opens
     };
   
-    const handleCloseModal = () => {
-      setIsModalOpen(false);
-      setIsExpanded(false); // Set expanded to false when the modal closes
-    };
+    // const handleCloseModal = () => {
+    //   setIsModalOpen(false);
+    //   setIsExpanded(false); // Set expanded to false when the modal closes
+    // };
   return (
     <div className="">
       <span
@@ -24,7 +25,7 @@ const AuctionList: React.FC = () => {
         Auction {isExpanded ? <ChevronUp className="h-4 w-4 text-black" /> : <ChevronDown className="h-4 w-4 text-black"/>}
       </span>
       <div>
-      <AuctionModal isOpen={isModalOpen} onClose={handleCloseModal} />
+      {/* <AuctionModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
       </div>
       
     </div>
